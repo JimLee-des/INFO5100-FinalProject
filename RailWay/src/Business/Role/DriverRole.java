63632e5cc415f54fd728846f7902991d925b321e
Jojolucky <he.jiao@northeastern.edu>
2022-04-27 09:41:32 -0400

    + 10 roles classes

---
+++ RailWay/src/Business/Role/DriverRole.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import ui.Role.CommanderWorkAreaJPanel;
+
+
+public class DriverRole extends Role{
+    
+    public DriverRole(){
+        this.type = Role.RoleType.Driver;
+    }
+    
+    @Override
+    public JPanel createWorkArea(JPanel mainScreen, UserAccount userAccount, EcoSystem system) {
+        this.type = Role.RoleType.Driver;
+        return new CommanderWorkAreaJPanel(mainScreen, userAccount, system);
+    }
+    
+}
